94baa317c5f08362a63b3168e3c1475f31eb9636
hossainks <shakirhossain.qa@gmail.com>
2024-11-17 21:57:34 +0600

    Update workflows

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 // const url = "https://httpbin.test.k6.io/post";
-const hostname = `http://${__ENV.DOMAIN}`;
+const hostname = `https://${__ENV.DOMAIN}`;
 const url = `${hostname}/post`;
 
 export default function () {
