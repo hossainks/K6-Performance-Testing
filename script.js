bc1bf562e0f121e88b72e9b93afc0e8bb848fa70
hossainks <shakirhossain.qa@gmail.com>
2024-11-17 20:35:13 +0600

    Add Scripts

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import http from "k6/http";
-const url = "https://httpbin.test.k6.io/post";
+// const url = "https://httpbin.test.k6.io/post";
+const hostname = `http://${__ENV.DOMAIN}`;
+const url = `${hostname}/post`;
 
 export default function () {
   let response = http.post(url, "Hello world!");
